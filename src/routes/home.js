757c1b018765521336f4a4b2b5a13598bd561772
Daniel Segovia <dsegovia@gmail.com>
2022-07-17 12:29:25 -0300

    01 version

--- src/routes/home.js
+++ src/routes/home.js
@@ -5,7 +5,11 @@ const Op = Sequelize.Op;
 
 
 router.get('/', async (req, res) => {
-  res.json(await Products.findAll())
+    
+    const results = await Products.findAll()
+    if(results.length === 0) return res.status(204).json({data: {msj: "no results"}})
+
+    return res.status(200).json({data: {results}})
 })
 
 router.get('/search', async (req, res) => {
